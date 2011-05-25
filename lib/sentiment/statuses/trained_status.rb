3afe13c4891dc854dce7c9a8814cef04fc2b4c1f
Joe Root <joe@onlysix.co.uk>
2011-05-25 16:21:58 +0100

    Updated server code for new class structure

--- lib/sentiment/statuses/trained_status.rb
+++ lib/sentiment/statuses/trained_status.rb
@@ -8,9 +8,6 @@ module Status
     key :subjective, String             # takes values "u", "t", "f", "b"
     key :polarity, String               # takes values "u", "+", "-", "0"
     key :sentiment, Array               # takes array of strings
-
-    # Validations
-    validates_presence_of :subjective, :polarity, :sentiment
-
+    
   end
 end
--- lib/server/server.rb
+++ lib/server/server.rb
@@ -12,40 +12,45 @@ class App
     erb :map
   end
   
-  get '/admin/classify' do
-    @statuses = MicroblogStatus.all("classified_status" => nil)
-    erb :"admin/classify/classify"
+  get '/admin/train' do
+    @statuses = Status::Status.where(:trained_status => nil).sort(:created_at.desc)
+    erb :"admin/train/index"
   end
   
-  get '/admin/classify/search' do
+  get '/admin/train/search' do
     if not params[:q].nil?
-      ClassifiedStatus.fetch_from_twitter params[:q]
+      Scraper::TwitterSearch.scrape params[:q]
     end
-    redirect '/admin/classify'
+    redirect '/admin/train'
   end
   
-  get '/admin/classify/trained' do
-    @statuses = Status::Status.all("classified_status" => {"$ne" => nil})
-    erb :"admin/classify/trained"
+  get '/admin/train/trained' do
+    @statuses = Status::Status.where(:trained_status.ne => nil).sort(:created_at.desc)
+    erb :"admin/train/trained"
   end
   
-  post '/admin/classify/:id' do
-    c = ClassifiedStatus.find_by_id params[:id]
+  post '/admin/train/:id' do
+    s = Status::Status.find_by_id params[:id]
     saved = false;
-    if not c.nil?
-      c.subjective = params["subjective"] if not params["subjective"].nil?
-      c.polarity = params["polarity"] if not params["polarity"].nil?
-      if not params["sentiment"].nil?
-        c.sentiment = params["sentiment"].split(",").map{|s| s.strip}
+    if not s.nil?
+      if s.trained_status.nil?
+        s.trained_status = Status::TrainedStatus.new
+        s.save
       end
-      saved = c.save
+      t = s.trained_status
+      t.subjective = params["subjective"] if not params["subjective"].nil?
+      t.polarity = params["polarity"] if not params["polarity"].nil?
+      if not params["sentiment"].nil?
+        t.sentiment = params["sentiment"].split(",").map{|s| s.strip}
+      end
+      saved = s.save and t.save
     end
-    @json = {"success" => true}
+    @json = {"success" => saved}
     erb :"admin/json"    
   end
   
-  get '/admin/classify/:id/delete' do
-    c = ClassifiedStatus.find_by_id id
+  get '/admin/train/:id/delete' do
+    s = Status::Status.find_by_id id
     destroyed = c.destroy
     @json = {"success" => destroyed}
     erb :"admin/json"
