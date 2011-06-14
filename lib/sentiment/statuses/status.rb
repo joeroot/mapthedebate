5a476210db8be3e57377f00d52e1b81c95f13060
Joe Root <joe@onlysix.co.uk>
2011-06-14 15:17:45 +0100

    Added additional methods for identifying patterns to statuses

--- lib/sentiment/statuses/status.rb
+++ lib/sentiment/statuses/status.rb
@@ -32,7 +32,11 @@ module Status
     end
 
     def hashtags
-      self.parts_of_speech.select{|pos| pos["tag"] == "hsh"}
+      self.parts_of_speech.select{|pos| pos["meta"]["hash"]}
+    end
+    
+    def hashtag_words
+      self.parts_of_speech.select{|pos| pos["meta"]["hash"] and pos["word"] == pos["meta"]["original"]}
     end
 
     def urls
@@ -40,7 +44,7 @@ module Status
     end
     
     def mentions
-      self.parts_of_speech.select{|pos| pos["tag"] == "mntn"}
+      self.parts_of_speech.select{|pos| pos["meta"]["mention"]}
     end
     
     def adjectives
@@ -67,6 +71,10 @@ module Status
       not self.trained_status.nil?
     end
     
+    def clues
+      self.parts_of_speech.select{|p| Core::ClueFinder.is_clue? p["word"], p["tag"]}
+    end
+    
     def strong_subjective_clues
       self.parts_of_speech.select{|p| Core::ClueFinder.is_strong_clue? p["word"], p["tag"]}
     end
@@ -75,8 +83,22 @@ module Status
       self.parts_of_speech.select{|p| Core::ClueFinder.is_weak_clue? p["word"], p["tag"]}
     end
     
+    def polarised_clues
+      polarised = {:positive => [], :negative => []}
+      negate = false
+      self.parts_of_speech.each do |p|
+        negate = !negate if ["not", "n't", "never"].include? p["word"]
+        negate = false if ["pp", "ppc"].include? p["tag"]
+        positive = Core::ClueFinder.is_positive_clue? p["word"], p["tag"]
+        negative = Core::ClueFinder.is_negative_clue? p["word"], p["tag"]
+        polarised[:positive] << p if (!negate and positive) or (negate and negative)
+        polarised[:negative] << p if (!negate and negative) or (negate and positive)
+      end
+      return polarised
+    end
+    
     def positive_clues
-      self.parts_of_speech.select{|p| Core::ClueFinder.is_positive_clue? p["word"], p["tag"]}
+      self.polarised_clues[:positive]
     end
     
     def strong_positive_clues
@@ -88,7 +110,7 @@ module Status
     end
     
     def negative_clues
-      self.parts_of_speech.select{|p| Core::ClueFinder.is_negative_clue? p["word"], p["tag"]}
+      self.polarised_clues[:negative]
     end
     
     def strong_negative_clues
@@ -97,6 +119,48 @@ module Status
     
     def weak_negative_clues
       self.negative_clues.select{|p| Core::ClueFinder.is_weak_clue? p["word"], p["tag"]}
+    end
+    
+    def patterns
+      rules = [
+        ["adj", "noun", nil],
+        ["adverb", "adj", "noun"],
+        ["adj", "adj", "noun"],
+        ["noun", "adj", "noun"],
+        ["adverb", "verb", nil]
+      ]
+      
+      pos = self.parts_of_speech
+      triplets = (pos + [nil]).each_cons(3).to_a    
+      
+      patterns = triplets.select do |first, second, third|
+        rules.map do |rule|
+          rule[0] == TweetTagger.general(first["tag"]) and 
+          rule[1] == TweetTagger.general(second["tag"]) and 
+          (
+            !rule[2] or 
+            (!third.nil? and rule[2] != TweetTagger.general(third["tag"]))
+          )
+        end.inject(false){|m,n| m or n}
+      end
+      
+      return patterns
+    end
+    
+    def patterns_with_positive_clues
+      positive = self.positive_clues
+      self.patterns.select do |s|
+        # s.map{|w| !w.nil? and Core::ClueFinder.is_positive_clue?(w["word"], w["tag"])}.inject(false){|m,n| m or n}
+        s.map{|w| !w.nil? and positive.include?(w)}.inject(false){|m,n| m or n}
+      end
+    end
+    
+    def patterns_with_negative_clues
+      negative = self.negative_clues
+      self.patterns.select do |s|
+        # s.map{|w| !w.nil? and Core::ClueFinder.is_negative_clue? w["word"], w["tag"]}.inject(false){|m,n| m or n}
+        s.map{|w| !w.nil? and negative.include?(w)}.inject(false){|m,n| m or n}
+      end
     end
     
   end
